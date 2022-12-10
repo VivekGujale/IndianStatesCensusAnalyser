610f58b7dacf2ebb8f4441eb2855512e7d0a8f2b
VivekGujale <vdgujale007@.com>
2022-12-10 18:45:13 +0530

    first commit

---
+++ src/main/java/org/example/IndianStatesCensus/IndianStatesCensusAnalyser.java
@@ -0,0 +1,7 @@
+package org.example.IndianStatesCensus;
+
+public class IndianStatesCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser");
+    }
+}
